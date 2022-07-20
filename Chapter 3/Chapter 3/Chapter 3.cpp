ef6b10c4ee17a12b73cc4c20d0f543f0c02ba768
MariaAlexisSalesABK <msales@activision.com>
2022-07-20 15:20:54 -0500

    chapter 3 commit

--- Chapter 3/Chapter 3/Chapter 3.cpp
+++ Chapter 3/Chapter 3/Chapter 3.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	int year;
+
+	cout << "Leap Year Checker" << endl;
+	cout << "Insert a year: ";
+	cin >> year;
+
+	if (year % 4 == 0) {
+		if (year % 100 == 0 && year % 400 != 0) {
+			cout << "Not a leap year" << endl;
+		} else {
+			cout << "Leap year!" << endl;
+		}
+	}
+
+	cout << "\n-------------------------------------\n" << endl;
+
+	unsigned int n;
+
+	cout << "Display all factors of a number:" << endl;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		if (n % i == 0) cout << i << " ";
+	}
+	
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
